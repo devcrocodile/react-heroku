327c91698e06b5c7823894d07ccf275afc291313
tudormth2 <tudormth2@gmail.com>
2020-01-02 06:32:15 +0200

    first

---
+++ build/precache-manifest.d3f04840c55b1379e4fcde7df7ec3164.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "990178b6906f1bb62ef2c3509d273d2f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b5641ebab54415bc91b9",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "2cd584ae14ac644110f4",
+    "url": "/static/js/2.a8e8816c.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.a8e8816c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b5641ebab54415bc91b9",
+    "url": "/static/js/main.45c7e0f8.chunk.js"
+  },
+  {
+    "revision": "f45055e140368b3856c0",
+    "url": "/static/js/runtime-main.70ae9e77.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
